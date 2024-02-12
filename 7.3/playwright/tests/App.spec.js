ea40046f5277c0b299faf8955a9763fbe8645a75
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2024-02-12 15:26:57 +0300

    7-03 задача 2 и 3

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -3,6 +3,31 @@ const { email, password } = require("../user.js");
 const { describe } = require("node:test");
 
 describe("Authorization", async ({ page }) => {
+  test("Success Authorization", async ({ page }) => {
+    await page.goto("https://netology.ru/?modal=sign_in");
+    await page.getByPlaceholder('Email').fill(email);
+    await page.getByPlaceholder('Пароль').fill(password);
+    await page.getByTestId('login-submit-btn').click();
+
+    await expect(page).toHaveURL("https://netology.ru/profile/8698862");
+
+    await page.waitForSelector("h2");
+
+    const title = await page.$eval("h2", (element) => element.textContent);
+
+    expect(title).toBe("Моё обучение");
+  });
+
+  test("Failed Authorization", async ({ page }) => {
+    await page.goto("https://netology.ru/?modal=sign_in");
+    await page.getByPlaceholder('Email').fill("test@111.ru");
+    await page.getByPlaceholder('Пароль').fill("password");
+    await page.getByTestId('login-submit-btn').click();
+
+    const title = await page.$eval("[data-testid='login-error-hint']", (element) => element.textContent);
+
+    expect(title).toBe("Вы ввели неправильно логин или пароль");
+  });
 
   test("Success Authorization With Screenshots", async ({ page }) => {
     await page.goto("https://netology.ru/?modal=sign_in");
