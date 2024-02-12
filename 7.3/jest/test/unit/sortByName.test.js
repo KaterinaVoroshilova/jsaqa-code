7dada2287e818ce5018ce444c6b631aa1c05e405
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2024-02-12 12:23:24 +0300

    7-03 задача 1 jest

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,37 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const result = sorting.sortByName(input);
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+    expect(result).toEqual(expected);
+  });
+
+  it("Books names should not be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const result = sorting.sortByName(input);
+    const expected = [
+      "Властелин Колец",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+    expect(result).toEqual(expected);
   });
 });
+
